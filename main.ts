726e64b0bb31fccb56c9a44f605b447e7d75d549
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-12-18 10:25:08 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 led.enable(false)
 let GripperStatus = 0
 let GripperRelease = 1
+let gripperState = 0;
 let winchInner = AnalogPin.P0
 let winchOuter = AnalogPin.P1
 let rotor = AnalogPin.P2
@@ -8,8 +9,7 @@ let gripper = AnalogPin.P3
 let SERVO_STOP = 90
 let GRIPPER_RELEASED = 130  // 180
 let GRIPPER_CLOSED = 40     // 90
-// basic.clearScreen()
-radio.setGroup(81)
+radio.setGroup(26)
 pins.servoWritePin(winchInner, SERVO_STOP)
 pins.servoWritePin(winchOuter, SERVO_STOP)
 pins.servoWritePin(rotor, SERVO_STOP)
@@ -21,31 +21,39 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "outer") {
         pins.servoWritePin(winchOuter, value)
     } else if (name == "gripper") {
-        if (value == 1) {
-            if (GripperRelease == 1) {
-                GripperStatus = 1
-                GripperRelease = 0
-            }
-        } else {
-            GripperRelease = 1
-        }
+        gripperState = value;
+        // if (value == 1) {
+        //     if (GripperRelease == 1) {
+        //         GripperStatus = 1
+        //         GripperRelease = 0
+        //     }
+        // } else {
+        //     GripperRelease = 1
+        // }
     }
 })
+
 basic.forever(function () {
-    if (GripperStatus == 1) {
-        pins.servoWritePin(gripper, GRIPPER_CLOSED)
-        // basic.showIcon(IconNames.Chessboard)
-        basic.pause(4000)
-        for (let index = 0; index < 6; index++) {
-            // basic.clearScreen()
-            basic.pause(400)
-            // basic.showIcon(IconNames.Chessboard)
-            basic.pause(400)
-        }
-        GripperStatus = 0
+    if (gripperState === 1) {
+        pins.servoWritePin(gripper, GRIPPER_RELEASED);
     } else {
-        // basic.clearScreen()
-        // basic.showIcon(IconNames.Chessboard)
-        pins.servoWritePin(gripper, GRIPPER_RELEASED)
+        pins.servoWritePin(gripper, GRIPPER_CLOSED);
     }
+
+    // if (GripperStatus == 1) {
+    //     pins.servoWritePin(gripper, GRIPPER_CLOSED)
+    //     // basic.showIcon(IconNames.Chessboard)
+    //     basic.pause(4000)
+    //     for (let index = 0; index < 6; index++) {
+    //         // basic.clearScreen()
+    //         basic.pause(400)
+    //         // basic.showIcon(IconNames.Chessboard)
+    //         basic.pause(400)
+    //     }
+    //     GripperStatus = 0
+    // } else {
+    //     // basic.clearScreen()
+    //     // basic.showIcon(IconNames.Chessboard)
+    //     pins.servoWritePin(gripper, GRIPPER_RELEASED)
+    // }
 })
